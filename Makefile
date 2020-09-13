e744045780f8a23dbc9392c102f3556a2ddd071b
Goran Mekić <meka@tilda.center>
2020-09-13 12:13:49 +0200

    Newer freenit

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+USE_FREENIT = YES
 REGGAE_PATH = /usr/local/share/reggae
 SERVICES = backend https://github.com/bsidesns/backend \
 	   frontend https://github.com/bsidesns/frontend
@@ -16,21 +17,5 @@ publish: collect
 
 shell: up
 	@${MAKE} ${MFLAGS} -C services/backend shell
-
-do_devel: up
-.if defined(service)
-.if defined(offline)
-	@${MAKE} ${MAKEFLAGS} -C services/${service} devel offline=${offline}
-.else
-	@${MAKE} ${MAKEFLAGS} -C services/${service} devel
-.endif
-.else
-.if defined(offline)
-	@env OFFLINE=${offline} SYSPKG=${SYSPKG} bin/devel.sh reggae
-.else
-	@env SYSPKG=${SYSPKG} bin/devel.sh reggae
-.endif
-.endif
-
 
 .include <${REGGAE_PATH}/mk/project.mk>
--- bin/devel.sh
+++ bin/devel.sh
@@ -5,6 +5,8 @@ export BIN_DIR=`dirname $0`
 export PROJECT_ROOT="${BIN_DIR}/.."
 . "${PROJECT_ROOT}/services/backend/name.py"
 export backend_app_name=${app_name}
+. "${PROJECT_ROOT}/services/frontend/name.ini"
+export frontend_app_name=${app_name}
 export OFFLINE="${OFFLINE:=no}"
 export REGGAE="no"
 
@@ -13,19 +15,18 @@ if [ "${1}" = "reggae" ]; then
   REGGAE="yes"
 fi
 
-cd "${PROJECT_ROOT}"
+
 if [ "${REGGAE}" = "yes" ]; then
   backend_hostname=$(sudo cbsd jexec user=devel "jname=${backend_app_name}" hostname)
-  make offline=${OFFLINE} SYSPKG=${SYSPKG} -C services/backend init
-  sudo tmux new-session -s "${backend_app_name}" -d "make offline=${OFFLINE} SYSPKG=${SYSPKG} -C services/backend devel"
-  sudo tmux split-window -h -p 50 -t 0 "make OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 -C services/frontend devel"
+  frontend_hostname=$(sudo cbsd jexec user=devel "jname=${frontend_app_name}front" hostname)
+  sudo tmux new-session -s "${backend_app_name}" -d "make -C services/backend devel"
+  sudo tmux split-window -h -p 50 -t 0 "make -C services/frontend BACKEND_URL=http://${backend_hostname}:5000 devel || sleep 10"
   sudo tmux a -t "${backend_app_name}"
-
 else
   backend_hostname='localhost'
   "${BIN_DIR}/download_repos.sh"
-  env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} "${PROJECT_ROOT}/services/backend/bin/init.sh"
-  tmux new-session -s "${backend_app_name}" -d "env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} ${PROJECT_ROOT}/services/backend/bin/devel.sh"
-  tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 ${PROJECT_ROOT}/services/frontend/bin/devel.sh"
+  env OFFLINE=${OFFLINE} "${PROJECT_ROOT}/services/backend/bin/init.sh"
+  tmux new-session -s "${backend_app_name}" -d "env OFFLINE=${OFFLINE} ${PROJECT_ROOT}/services/backend/bin/devel.sh"
+  tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} BACKEND_URL=\"http://${backend_hostname}:5000\" ${PROJECT_ROOT}/services/frontend/bin/devel.sh"
   tmux a -t "${backend_app_name}"
 fi
